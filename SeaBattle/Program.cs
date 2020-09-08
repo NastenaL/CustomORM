1aea1c6bf18bdcedc1dc9464ec35940ff2686275
Настя Настя <ipoet@mail.ru>
2020-09-08 10:46:50 +0300

    rewrite shipType. Add to main method repositories

--- SeaBattle/Program.cs
+++ SeaBattle/Program.cs
@@ -25,22 +25,32 @@
                 Description = "Mix"
             };
 
+            Repository<ShipType> shipTypeRepository = new Repository<ShipType>();
+            shipTypeRepository.Insert(military);
+            shipTypeRepository.Insert(auxiliary);
+            shipTypeRepository.Insert(mix);
+
             AuxiliaryShip auxiliaryShip = new AuxiliaryShip
             {
                 Dx = 1,
                 Dy = 0,
                 Range = 1,
-                Length = 2
+                Length = 2,
+                ShipTypeId = 2
             };
             MilitaryShip militaryShip = new MilitaryShip
             {
                 Dx = 1,
                 Dy = 0,
                 Range = 1,
-                Length = 2
+                Length = 2,
+                ShipTypeId = 1
             };
-           
-            
+
+            Repository<Ship> shipRepository = new Repository<Ship>();
+            shipRepository.Insert(militaryShip);
+            shipRepository.Insert(auxiliaryShip);
+
             Console.ReadKey();
         }
     }
