8930721994bb684d878f5075c0cebaa80af0b2d3
Настя Настя <ipoet@mail.ru>
2020-09-14 11:32:02 +0300

    add attribute class

---
+++ SeaBattle.ORM/RedefineColumnAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace SeaBattle.ORM
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RedefineColumnAttribute : Attribute
+    {
+        public string newName;
+        public RedefineColumnAttribute(string newName)
+        {
+            this.newName = newName;
+        }
+    }
+}
